16514653089f229995dc21791b9e6a101068d664
naisenyamungai <naisenyamungai@gmail.com>
2019-05-17 13:11:07 +0300

    test file modified

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,15 +1,3 @@
-import org.junit.*;
-import static org.junit.Assert.*;
 
-public class AppTest {
 
-    @Test
-    public void runApp_displayIndexOf_numberValue() {
-        String oneLetter = "A";
-        Integer actual = 0;
-        Integer expected = oneLetter.indexOf("A");
-        assertEquals(expected, actual);
-
-    }
-}
 
